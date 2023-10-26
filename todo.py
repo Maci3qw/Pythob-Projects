4340ccc988c30905a327076ecc7966bd4e6ceedb
Maciej Wojcuła <Maciej.Wojcula@leadec-services.com.pl>
2023-10-26 12:02:03 +0200

    Based architecture for To-do app, already deployed two functions with simple validation

---
+++ todo.py
@@ -0,0 +1,77 @@
+import datetime
+import time
+import os
+
+class todo:
+        
+        def __init__(self, lista_zadan=None):
+                self.lista_zadan = [] 
+
+        def dodaj_zadanie(self):
+                print("Dodajemy zadanie")
+                zadanie = input("Podaj zadanie: ")
+                self.lista_zadan.append(zadanie)
+                print("Pomyślnie dodano zadanie!")
+                time.sleep(1)
+
+        def wyswietl_zadania(self):
+                print("Oto twoje aktualne zadania:")
+                
+                if not self.lista_zadan:
+                            print("Twoja lista jest pusta")
+                            nowe_zadanie = input("Czy chcesz coś dodać?")
+                            match nowe_zadanie:
+                                    case "tak":
+                                            self.dodaj_zadanie()
+                                            print("Przenoszę do sekcji dodawania zadań..")
+                                            time.sleep(1)
+
+                                    case "nie":
+                                            return
+                                            
+                            time.sleep(2)
+                else:
+                    print(self.lista_zadan)
+                    time.sleep(2)
+
+        def oznaczanie(self):
+                print("Wykonane / Nie wykonane")
+
+        def zapisz_do_pliku(self):
+                print("Zapisuje do pliku")
+        
+        def odczytaj(self):
+                print("Odczytuje plik...")
+        
+        def wyjdz(self):
+                print("Do widzenia!")
+                exit()
+while True:       
+    print("Witamy w programie Notes2024!")
+    print("----------")
+    print("1: Dodaj zadanie" )
+    print("2: Wyświetl liste zadań" )
+    print("3: Oznacz jako wykonane" )
+    print("4: Zapisz do pliku" )
+    print("5: Odczytaj plik" )
+    print("6: Wyjdź")
+    print("----------")
+    question = input("Co byś chciał dzisiaj zrobić?") 
+
+    my_todo = todo()
+
+    match question:
+            case "1":
+                    my_todo.dodaj_zadanie()
+            
+            case "2":
+                    my_todo.wyswietl_zadania()
+                
+            case "3":
+                my_todo.oznaczanie()
+            case "4":        
+                    my_todo.zapisz_do_pliku()
+            case "5":
+                    my_todo.odczytaj()
+            case "6":
+                    my_todo.wyjdz()
